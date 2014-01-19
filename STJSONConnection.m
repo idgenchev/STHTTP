c162986bf273f74ac1579b5800b9288bc4510432
Ivan Genchev <ivan.genchev@steppinstonez.com>
2014-01-19 18:37:24 +0000

    Don't try to parse the response if there was an error loading the data.

--- STJSONConnection.m
+++ STJSONConnection.m
@@ -5,9 +5,11 @@
 
 - (void)didFinishLoading {
     self.loadingData = NO;
-    NSError *error = nil;
-    self.json = [NSJSONSerialization JSONObjectWithData:self.data options:NSJSONReadingMutableContainers error:&error];
-    self.error = error;
+    if (!self.error) {
+        NSError *error = nil;
+        self.json = [NSJSONSerialization JSONObjectWithData:self.data options:NSJSONReadingMutableContainers error:&error];
+        self.error = error;
+    }
     if (self.completionHandler) { self.completionHandler(self); }
     if (self.receiver) { [self.receiver performSelector:self.action withObject:self]; }
 }
