dfbf93a99af3446085f68c184a69898d132b3602
Ivan Genchev <ivan.genchev@steppinstonez.com>
2014-01-19 15:23:42 +0000

    Handle requests with multipart/form-data Content-Type.

--- STHTTPConnection.m
+++ STHTTPConnection.m
@@ -19,6 +19,7 @@ BOOL isContainer(Class class) {
 @property (nonatomic, strong) NSMutableData *mutableData;
 
 - (NSString*)requestBodyFromDictionary:(NSDictionary*)parameters;
+- (void)setMultipartRequestBody:(NSMutableURLRequest*)request fromDictionary:(NSDictionary*)parameters;
 
 @end
 
@@ -98,21 +99,29 @@ BOOL isContainer(Class class) {
     self.mutableData = [NSMutableData data];
     
     NSMutableURLRequest *mutableRequest;
-    if (self.request.HTTPMethod &&
-        ([self.request.HTTPMethod isEqualToString:@"PUT"] || [self.request.HTTPMethod isEqualToString:@"POST"])) {
+    
+    if ([self.httpHeaderFields[@"Content-Type"] isEqualToString:@"multipart/form-data"]) {
         mutableRequest = [NSMutableURLRequest requestWithURL:self.request.URL cachePolicy:self.cachePolicy timeoutInterval:self.timeoutInterval];
-        [mutableRequest setHTTPBody:[[self requestBodyFromDictionary:self.parameters] dataUsingEncoding:NSUTF8StringEncoding]];
+        [self setMultipartRequestBody:mutableRequest fromDictionary:self.parameters];
+        mutableRequest.HTTPMethod = self.request.HTTPMethod ? self.request.HTTPMethod : @"POST";
     } else {
-        NSString *urlString = [NSString stringWithFormat:@"%@?%@", [self.request.URL absoluteString], [self requestBodyFromDictionary:self.parameters]];
-        mutableRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString] cachePolicy:self.cachePolicy timeoutInterval:self.timeoutInterval];
-        [mutableRequest setHTTPMethod:self.request.HTTPMethod ? self.request.HTTPMethod : @"GET"];
+        if (self.request.HTTPMethod &&
+            ([self.request.HTTPMethod isEqualToString:@"PUT"] || [self.request.HTTPMethod isEqualToString:@"POST"])) {
+            mutableRequest = [NSMutableURLRequest requestWithURL:self.request.URL cachePolicy:self.cachePolicy timeoutInterval:self.timeoutInterval];
+            [mutableRequest setHTTPBody:[[self requestBodyFromDictionary:self.parameters] dataUsingEncoding:NSUTF8StringEncoding]];
+        } else {
+            NSString *urlString = [NSString stringWithFormat:@"%@?%@", [self.request.URL absoluteString], [self requestBodyFromDictionary:self.parameters]];
+            mutableRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:urlString] cachePolicy:self.cachePolicy timeoutInterval:self.timeoutInterval];
+            [mutableRequest setHTTPMethod:self.request.HTTPMethod ? self.request.HTTPMethod : @"GET"];
+        }
     }
+    
     if (self.httpHeaderFields.count > 0) {
         [self.httpHeaderFields enumerateKeysAndObjectsUsingBlock:^(NSString *headerField, NSString *value, BOOL *stop) {
-                [mutableRequest setValue:value forHTTPHeaderField:headerField];
-            }];
+            [mutableRequest setValue:value forHTTPHeaderField:headerField];
+        }];
     }
-
+    
     self.request = mutableRequest;
     self.connection = [[NSURLConnection alloc] initWithRequest:self.request delegate:self startImmediately:YES];
 }
@@ -212,6 +221,32 @@ BOOL isContainer(Class class) {
     }
 	
     return body;
+}
+
+- (void)setMultipartRequestBody:(NSMutableURLRequest*)request fromDictionary:(NSDictionary*)parameters {
+    NSString *boundary = @"0xB0un9ArY";
+    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
+    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+    [request setValue:@"1.0" forHTTPHeaderField:@"MIME-Version"];
+    [request setValue:@"keep-alive" forHTTPHeaderField:@"Connection"];
+    
+    NSMutableData *body = [NSMutableData data];
+    
+    [parameters enumerateKeysAndObjectsUsingBlock:^(NSString *key, id obj, BOOL *stop) {
+        [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        if ([obj isKindOfClass:[NSString class]]) {
+            [body appendData:[[NSString stringWithFormat:@"Content-Type: text/plain\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
+            [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+            [body appendData:[[NSString stringWithFormat:@"%@\r\n", obj] dataUsingEncoding:NSUTF8StringEncoding]];
+        } else {
+            [body appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n", obj[@"Content-Type"]] dataUsingEncoding:NSUTF8StringEncoding]];
+            [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
+            [body appendData:obj[@"data"]];
+        }
+    }];
+    
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [request setHTTPBody:body];
 }
 
 #pragma mark -
